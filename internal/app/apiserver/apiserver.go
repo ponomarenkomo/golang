66dee681a4715da155156d50f64161f988bd1b60
ponomarenkomo <ponomarenko.n.2001@gmail.com>
2023-05-23 09:39:47 +0300

    web

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -4,11 +4,13 @@ import (
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/sirupsen/logrus"
 )
 
 func Start(config *Config) error {
 
 	srv := httprouter.New()
+	logrus.Info("Starting server!!!")
 
 	return http.ListenAndServe(config.BindAddr, srv)
 
